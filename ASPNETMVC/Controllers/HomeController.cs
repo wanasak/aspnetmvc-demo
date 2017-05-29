28596b4596230ebfd42034a6529d9b79ab6db811
wanasak <u510610433@gmail.com>
2017-05-29 13:57:32 +0700

    hyperlink using actionlink html helper

--- ASPNETMVC/Controllers/HomeController.cs
+++ ASPNETMVC/Controllers/HomeController.cs
@@ -10,19 +10,14 @@ namespace ASPNETMVC.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index(int id, string name)
+        public ActionResult Index()
         {
-            ViewBag.Message = $"Your id is {id} and name is {Request.QueryString["name"]}";
+            //ViewBag.Message = $"Your id is {id} and name is {Request.QueryString["name"]}";
             ViewBag.Cars = new List<string>() { "Toyota", "Honda", "BMW" };
             ViewData["Numbers"] = new List<int>() { 1, 2, 3 };
-            Employee employee = new Employee()
-            {
-                EmployeeId = 1,
-                Name = "Smudger",
-                Gender = "Male",
-                City = "Chiang Mai"
-            };
-            return View(employee);
+            EmployeeContext context = new EmployeeContext();
+            IEnumerable<Employee> employees = context.Employees.ToList();
+            return View(employees);
         }
 
         public ActionResult Details(int id)
